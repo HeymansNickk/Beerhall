30de00ad64c9fb6338a72767e1d59e26cf5517c1
Nick Heymans <nick.heymans@hotmail.com>
2020-08-13 11:02:32 +0200

    Configure EF and add database Initialization

---
+++ Beerhall/Data/Mappers/BeerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Beerhall.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Beerhall.Data.Mappers
+{
+    public class BeerConfiguration : IEntityTypeConfiguration<Beer>
+    {
+        public void Configure(EntityTypeBuilder<Beer> builder)
+        {
+            builder.ToTable("Beer");
+
+            builder.Property(b => b.Name).IsRequired().HasMaxLength(100);
+        }
+    }
+}
