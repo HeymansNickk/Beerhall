2afb08cb3ddf090d3ccba145a7c88e9f16135f62
Nick Heymans <nick.heymans@hotmail.com>
2020-08-13 11:34:01 +0200

    add repos

---
+++ Beerhall/Data/Repository/BrewerRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Beerhall.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beerhall.Data.Repository
+{
+    public class BrewerRepository : IBrewerRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<Brewer> _brewers;
+
+        public BrewerRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _brewers = dbContext.Brewers;
+        }
+
+        public void Add(Brewer brewer)
+        {
+            _brewers.Add(brewer);
+        }
+
+        public void Delete(Brewer brewer)
+        {
+            _brewers.Remove(brewer);
+        }
+
+        public IEnumerable<Brewer> GetAll()
+        {
+            return _brewers.Include(b => b.Location).ToList();
+        }
+
+        public Brewer GetBy(int brewerID)
+        {
+            return _brewers.SingleOrDefault(b => b.BrewerId == brewerID);
+        }
+
+        public Brewer GetByWithBeers(int brewerId)
+        {
+            return _brewers.Include(b => b.Beers).SingleOrDefault(b => b.BrewerId == brewerId);
+        }
+
+        public IEnumerable<Brewer> GetAllWithBeers()
+        {
+            return _brewers.Include(b => b.Location).Include(b => b.Beers).ToList();
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
